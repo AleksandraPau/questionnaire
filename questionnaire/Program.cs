020bd299e3bee6811635cca7703c84ca62c3a81e
Александра Паустовская <sahsa.com1997@gmail.com>
2021-06-16 12:16:40 +0300

    Добавьте файлы проекта.

---
+++ questionnaire/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace questionnaire
+{
+    class Program
+    {
+		public static void Main(string[] args)
+		{
+
+			string MyName = "Sasha";
+			byte MyAge = 21;
+			bool HaveIApet = true;
+			double MyShoeSize = 40.5;
+
+			Console.WriteLine("My name is " + MyName);
+			Console.WriteLine("MyAge " + MyAge);
+			Console.WriteLine("Do I have a pet? " + HaveIApet);
+			Console.WriteLine("My shoe size is " + MyShoeSize);
+
+		}
+	}
+}
