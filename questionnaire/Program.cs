207e5f0140a8e4684bc6afef13b7e46625979ef9
Александра Паустовская <sahsa.com1997@gmail.com>
2021-07-02 07:35:43 +0300

    новый вариант с отдельными модулями и хранением информации

--- questionnaire/Program.cs
+++ questionnaire/Program.cs
@@ -4,21 +4,56 @@ namespace questionnaire
 {
     class Program
     {
+		static (string Name, string LastName, int Age, bool IsHavePet) EnterUser()
+        {
+			(string Name, string LastName, int Age, bool IsHavePet) User;
+			Console.WriteLine("Введите имя");
+			User.Name = Console.ReadLine();
+
+			Console.WriteLine("Введите фамилию");
+			User.LastName = Console.ReadLine();
+
+			string age;
+			int intage;
+			do
+			{
+				Console.WriteLine("Введите возраст цифрами");
+				age = Console.ReadLine();
+			} while (CheckNum(age, out intage));
+
+			Console.WriteLine("Имеете ли Вы питомца(ев)? (Да/Нет)");
+			if (Console.ReadLine() == "Да")
+            {
+				User.IsHavePet = true;
+				
+            }	
+			else
+            {
+				User.IsHavePet = false;
+            }
+
+			return User;
+        }
+
+		static bool CheckNum(string number, out int correctnumber)
+        {
+			if(int.TryParse(number, out int intnum))
+            {
+				if (intnum > 0)
+				{
+					correctnumber = intnum;
+					return true;
+				}
+            }
+            {
+				correctnumber = 0;
+				return false;
+            }
+        }
+
 		public static void Main(string[] args)
 		{
-			Console.Write("Enter your name: ");
-			var Name = Console.ReadLine();
-
-			Console.Write("Enter your age: ");
-			var age = Convert.ToInt32(Console.ReadLine());
-
-			Console.WriteLine("Your name is {0} and age is {1}", Name, age);
-
-			Console.Write("Enter your birthdate: ");
-
-			var birthdate = Console.ReadLine();
-
-			Console.WriteLine("Your birthdate is {0}", birthdate);
+			
 		}
 	}
 }
